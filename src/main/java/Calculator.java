aa7e481bf04c09cdeb0f0c57220513e0f876321c
rajkumarjakkula <rajkumarjakkula969@gmail.com>
2021-08-21 19:25:42 +0530

    Calculator Testing using Mockito

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,13 @@
 public class Calculator {
+
+    ArithmeticOperations arithmetic;
+
+    public Calculator (ArithmeticOperations arithmetic){
+        this.arithmetic = arithmetic;
+
+    }
     public double sum(double input1, double input2) {
-        return input1+input2;
+        return arithmetic.sum(input1,input2);
     }
     public double subtract(double input1, double input2) {
         return input1-input2;
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,41 +2,40 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculatorTest {
-    private Calculator calc=null;
+    Calculator calc=null;
+    ArithmeticOperations arithmetic=mock(ArithmeticOperations.class);
     @BeforeEach
     void assignObject()
     {
-        calc=new Calculator();
+        calc=new Calculator(arithmetic);
     }
     @Test
     void toReturnSumOfTwoPositiveValues()
     {
-        double actualOutput=calc.sum(4.0,4.0);
-        double expectedOutput=8.0;
-        assertEquals(actualOutput,expectedOutput);
+        when(arithmetic.sum(4.0,4.0)).thenReturn(8.0);
+        assertEquals(8.0,calc.sum(4.0,4.0));
     }
 
     @Test
     void toReturnDifferenceOfTwoPositiveValues()
     {
-        double actualOutput=calc.subtract(4.0,2.0);
-        double expectedOutput=2.0;
-        assertEquals(actualOutput,expectedOutput);
+        when(arithmetic.subtract(4.0,2.0)).thenReturn(2.0);
+        assertEquals(2.0,calc.subtract(4.0,2.0));
     }
     @Test
     void toReturnMultiplyOfTwoPositiveValues()
     {
-        double actualOutput=calc.multiply(4.0,4.0);
-        double expectedOutput=16.0;
-        assertEquals(actualOutput,expectedOutput);
+        when(arithmetic.multiply(4.0,4.0)).thenReturn(16.0);
+        assertEquals(16.0,calc.multiply(4.0,4.0));
     }
     @Test
     void toReturnDivisionOfTwoPositiveValues()
     {
-        double actualOutput=calc.divide(4.0,2.0);
-        double expectedOutput=2.0;
-        assertEquals(actualOutput,expectedOutput);
+        when(arithmetic.divide(4.0,2.0)).thenReturn(2.0);
+        assertEquals(2.0,calc.divide(4.0,2.0));
     }
 }
