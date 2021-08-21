2304f80d5a8e1b2f419979d342c4136b10f0b4c8
rajkumarjakkula <rajkumarjakkula969@gmail.com>
2021-08-21 19:07:12 +0530

    Basic Calculator Testing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,14 @@
 public class Calculator {
+    public double sum(double input1, double input2) {
+        return input1+input2;
+    }
+    public double substract(double input1, double input2) {
+        return input1-input2;
+    }
+    public double multiply(double input1, double input2) {
+        return input1*input2;
+    }
+    public double divide(double input1, double input2) {
+        return input1/input2;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    private Calculator calc=null;
+    @BeforeEach
+    void assignObject()
+    {
+        calc=new Calculator();
+    }
+    @Test
+    void toReturnSumOfTwoPositiveValues()
+    {
+        double actualOutput=calc.sum(4.0,4.0);
+        double expectedOutput=8.0;
+        assertEquals(actualOutput,expectedOutput);
+    }
+
+    @Test
+    void toReturnDifferenceOfTwoPositiveValues()
+    {
+        double actualOutput=calc.substract(4.0,2.0);
+        double expectedOutput=2.0;
+        assertEquals(actualOutput,expectedOutput);
+    }
+    @Test
+    void toReturnMultiplyOfTwoPositiveValues()
+    {
+        double actualOutput=calc.multiply(4.0,4.0);
+        double expectedOutput=16.0;
+        assertEquals(actualOutput,expectedOutput);
+    }
+    @Test
+    void toReturnDivisionOfTwoPositiveValues()
+    {
+        double actualOutput=calc.divide(4.0,2.0);
+        double expectedOutput=2.0;
+        assertEquals(actualOutput,expectedOutput);
+    }
 }
